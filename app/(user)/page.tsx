4691aa53a91b9fb3d64ef6b0806ef19f6a285c9a
MozartOnWeb <mozartontheweb@gmail.com>
2023-04-05 12:02:04 +0000

    resetting all layout styles

--- app/(user)/page.tsx
+++ app/(user)/page.tsx
@@ -1,11 +1,3 @@
 export default function Home() {
-  return (
-    <main className="home-page">
-      <div className="home-header" />
-
-      <section className="home-main">
-        <h1>home main section</h1>
-      </section>
-    </main>
-  );
+  return <main className="home-page"></main>;
 }
