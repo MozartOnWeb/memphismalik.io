97f8969fc46fdb4747fcda173a916e1aff1c8fdd
Mandjou Dama <79732869+MozartOnWeb@users.noreply.github.com>
2023-08-13 16:01:41 +0000

    make responsive writing page

--- components/writing_sidebar/WritingSidebar.tsx
+++ components/writing_sidebar/WritingSidebar.tsx
@@ -4,7 +4,32 @@ import BlogLink from "../blog_link/BlogLink";
 //import icons
 import { SubscribeIcon } from "@/public/assets/icons";
 
-export default function WritingSidebar() {
+const writingLinks = [
+  {
+    href: "/writings/1",
+    title: "Design critique for fund and profit",
+    date: "February 19, 2023",
+  },
+  {
+    href: "/writings/2",
+    title: "The Side Project Prophecy",
+    date: "January 17, 2022",
+  },
+  {
+    href: "/writings/3",
+    title: "What I've learned so far about design advising and angel investing",
+    date: "July 18, 2021",
+  },
+  {
+    href: "/writings/4",
+    title: "Make a personal changelog",
+    date: "April 29, 2021",
+  },
+];
+
+const wait = (ms: number) => new Promise((resolve) => setTimeout(resolve, ms));
+
+export default async function WritingSidebar() {
   return (
     <div className="writing_sidebar">
       <div className="headline">
@@ -16,81 +41,14 @@ export default function WritingSidebar() {
       </div>
 
       <div className="blog_links_container">
-        <BlogLink
-          href="/writings/1"
-          title="Design critique for fund and profit"
-          date="February 19, 2023"
-        />
-        <BlogLink
-          href="/"
-          title="The Side Project Prophecy"
-          date="January 17, 2022"
-        />
-        <BlogLink
-          href="/"
-          title="What I've learned so far about design advising and angel investing"
-          date="July 18, 2021"
-        />
-        <BlogLink
-          href="/"
-          title="Make a personal changelog"
-          date="April 29, 2021"
-        />
-        <BlogLink
-          href="/"
-          title="My playbook for shipping side projects"
-          date="January 30, 2021"
-        />
-        <BlogLink
-          href="/"
-          title="Design critique for fund and profit"
-          date="February 19, 2023"
-        />
-        <BlogLink
-          href="/"
-          title="The Side Project Prophecy"
-          date="January 17, 2022"
-        />
-        <BlogLink
-          href="/"
-          title="What I've learned so far about design advising and angel investing"
-          date="July 18, 2021"
-        />
-        <BlogLink
-          href="/"
-          title="Make a personal changelog"
-          date="April 29, 2021"
-        />
-        <BlogLink
-          href="/"
-          title="My playbook for shipping side projects"
-          date="January 30, 2021"
-        />
-        <BlogLink
-          href="/"
-          title="Design critique for fund and profit"
-          date="February 19, 2023"
-        />
-        <BlogLink
-          href="/"
-          title="The Side Project Prophecy"
-          date="January 17, 2022"
-        />
-        <BlogLink
-          href="/"
-          title="What I've learned so far about design advising and angel investing"
-          date="July 18, 2021"
-        />
-        <BlogLink
-          href="/"
-          title="Make a personal changelog"
-          date="April 29, 2021"
-        />
-        <BlogLink
-          href="/"
-          title="My playbook for shipping side projects"
-          date="January 30, 2021"
-        />
+        {writingLinks.map((link) => (
+          <BlogLink
+            key={link.href}
+            href={link.href}
+            title={link.title}
+            date={link.date}
+          />
+        ))}
       </div>
     </div>
   );
