dfc508cf805072fb3ed95c518e7fed87ef75d5e3
Mandjou Dama <79732869+MozartOnWeb@users.noreply.github.com>
2023-08-14 23:36:56 +0000

    fetching writings

--- components/writing_sidebar/WritingSidebar.tsx
+++ components/writing_sidebar/WritingSidebar.tsx
@@ -1,35 +1,24 @@
 //import components
 import BlogLink from "../blog_link/BlogLink";
 
+//import fetch writings
+import { fetchWritings } from "@/sanity/fetch";
+
 //import icons
 import { SubscribeIcon } from "@/public/assets/icons";
 
-const writingLinks = [
-  {
-    href: "/writings/1",
-    title: "Design critique for fund and profit",
-    date: "February 19, 2023",
-  },
-  {
-    href: "/writings/2",
-    title: "The Side Project Prophecy",
-    date: "January 17, 2022",
-  },
-  {
-    href: "/writings/3",
-    title: "What I've learned so far about design advising and angel investing",
-    date: "July 18, 2021",
-  },
-  {
-    href: "/writings/4",
-    title: "Make a personal changelog",
-    date: "April 29, 2021",
-  },
-];
+//define types
+type WritingsProps = {
+  title: string;
+  slug: string;
+  publishedAt: string;
+};
 
 const wait = (ms: number) => new Promise((resolve) => setTimeout(resolve, ms));
 
 export default async function WritingSidebar() {
+  const writings: WritingsProps[] = await fetchWritings();
+
   return (
     <div className="writing_sidebar">
       <div className="headline">
@@ -41,13 +30,8 @@ export default async function WritingSidebar() {
       </div>
 
       <div className="blog_links_container">
-        {writingLinks.map((link) => (
-          <BlogLink
-            key={link.href}
-            href={link.href}
-            title={link.title}
-            date={link.date}
-          />
+        {writings.map(({ title, slug, publishedAt }) => (
+          <BlogLink key={slug} href={slug} title={title} date={publishedAt} />
         ))}
       </div>
     </div>
