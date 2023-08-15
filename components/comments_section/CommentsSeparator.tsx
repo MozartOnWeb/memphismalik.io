a0ddda2b5fe06b7584d106bbdc0c8acf14cd791b
Mandjou Dama <79732869+MozartOnWeb@users.noreply.github.com>
2023-08-15 23:47:43 +0000

    fix bugs

--- components/comments_section/CommentsSeparator.tsx
+++ components/comments_section/CommentsSeparator.tsx
@@ -9,9 +9,9 @@ export default function CommentsSeparator() {
         viewBox="0 0 24 24"
         fill="none"
         stroke="#EFEFEF"
-        stroke-width="2"
-        stroke-linecap="round"
-        stroke-linejoin="round"
+        strokeWidth="2"
+        strokeLinecap="round"
+        strokeLinejoin="round"
         className="lucide lucide-message-circle"
       >
         <path d="m3 21 1.9-5.7a8.5 8.5 0 1 1 3.8 3.8z" />
